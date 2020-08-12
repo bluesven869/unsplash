15bb255518f636e0cec5c53278c9bc9070c7c439
bluesven869 <bluesven869@gmail.com>
2020-08-12 11:53:26 -0400

    remove AccessKey & SecretKey

--- src/constants/unsplash.js
+++ src/constants/unsplash.js
@@ -1,2 +1,6 @@
 import Unsplash from 'unsplash-js';
-export const unsplash = new Unsplash({ accessKey: "aa2f3c3be8125f1fc86e3007153420c4e446c19b7b0c6d80a6257b281c9a0dc5" });
+import {UNSPLASH_ACCESS_KEY, UNSPLASH_SECRET_KEY} from '@env';
+export const unsplash = new Unsplash({
+  accessKey: UNSPLASH_ACCESS_KEY,
+  secretKey: UNSPLASH_SECRET_KEY,
+});
